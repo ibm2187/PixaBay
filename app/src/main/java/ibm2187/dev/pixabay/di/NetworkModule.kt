38d80577731c58496328122f6b448c134ed38a39
Ibrahim Alshaikhli <32739489+ibm2187@users.noreply.github.com>
2022-09-04 21:29:16 -0400

    updated token implementation

--- app/src/main/java/ibm2187/dev/pixabay/di/NetworkModule.kt
+++ app/src/main/java/ibm2187/dev/pixabay/di/NetworkModule.kt
@@ -14,6 +14,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
@@ -74,7 +75,7 @@ class NetworkModule {
             val requestURL = request.url
 
             val requestQueryUrl = requestURL.newBuilder()
-                .addQueryParameter("key", "22987170-a645446e885845ec526c80906")
+                .addQueryParameter("key", BuildConfig.TOKEN_KEY)
                 .build()
 
             val requestBuilder = request.newBuilder()
